380326e80105739524ee43aa45bbd2c236ad700d
dulcelene machado <dudutola@gmail.com>
2024-06-19 14:03:08 +0200

    Added the placeholder for bank account information

--- designs/argent-bank/src/pages/UserProfile/index.jsx
+++ designs/argent-bank/src/pages/UserProfile/index.jsx
@@ -2,7 +2,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { useProfileUserMutation } from "../../services/userApi";
 import { useEffect } from "react";
 import { profileSlice } from "../../features/Profile/profileSlice";
-
 import { Profile } from "../../features/Profile";
 
 export const UserProfile = () => {
@@ -28,11 +27,42 @@ export const UserProfile = () => {
   const lastName = useSelector((state) => state.profile.lastName);
 
   return (
-    <main className="main bg-dark">
-      <div className="header">
+    <main classNameName="main bg-dark">
+      <div classNameName="header">
         <h1>Welcome back<br />{firstName} {lastName}!</h1>
         <Profile />
       </div>
+      <h2 className="sr-only">Accounts</h2>
+      <section className="account">
+        <div className="account__content--wrapper">
+          <h3 className="account__title">Argent Bank Checking (x8349)</h3>
+          <p className="account__amount">$2,082.79</p>
+          <p className="account__amount--description">Available Balance</p>
+        </div>
+        <div className="account__content--wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+      <section className="account">
+        <div className="account__content--wrapper">
+          <h3 className="account__title">Argent Bank Savings (x6712)</h3>
+          <p className="account__amount">$10,928.42</p>
+          <p className="account__amount--description">Available Balance</p>
+        </div>
+        <div className="account__content--wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+      <section className="account">
+        <div className="account__content--wrapper">
+          <h3 className="account__title">Argent Bank Credit Card (x8349)</h3>
+          <p className="account__amount">$184.30</p>
+          <p className="account__amount--description">Current Balance</p>
+        </div>
+        <div className="account__content--wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
     </main>
   );
 };

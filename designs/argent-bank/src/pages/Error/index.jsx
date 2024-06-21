a5ce4dfb9f2d992bb45e7d9b413dfbb4d34c1160
dulcelene machado <dudutola@gmail.com>
2024-06-21 11:41:10 +0200

    Implemented 404 error page with dynamic color-changing digits

--- designs/argent-bank/src/pages/Error/index.jsx
+++ designs/argent-bank/src/pages/Error/index.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+import "../../styles/pages/_error.scss";
 
 export const Error = () => {
   return (
-    <div className="error">
-      <h1>404</h1>
+    <main className="main bg-dark error">
+      <div>
+        <span className="digit">4</span>
+        <span className="digit">0</span>
+        <span className="digit">4</span>
+      </div>
       <p>Oops! The page you are looking for does not exist.</p>
-      <Link to="/">Go back to Home</Link>
-    </div>
+      <Link to="/" className="back">Go back to Home</Link>
+    </main>
   );
 };
